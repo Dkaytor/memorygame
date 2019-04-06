ab459d56750f0fd07609c9440a58a4ca5cc216a9
Dkaytor <counting1121@yahoo.com>
2019-04-06 14:09:31 -0400

    Updates

---
+++ precache-manifest.98fd00619d0a55b045d7c3d1b98ad0c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  },
+  {
+    "revision": "3707f7785681d8001132",
+    "url": "/memorygame/static/js/main.a4508763.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memorygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3707f7785681d8001132",
+    "url": "/memorygame/static/css/main.7766e51c.chunk.css"
+  },
+  {
+    "revision": "eb27871387e3969671fb05bf2bc56333",
+    "url": "/memorygame/index.html"
+  }
+];
